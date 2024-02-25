a7ad08c08ea4631c44ad4684340fb72f9fff6afc
eggpoison <edwrd.magee@gmail.com>
2024-02-26 10:41:47 +1100

    a

--- src/rendering/turret-range-rendering.ts
+++ src/rendering/turret-range-rendering.ts
@@ -26,8 +26,8 @@ const TURRET_RANGE_INFO_RECORD: Partial<Record<ItemType, TurretRangeInfo>> = {
    [ItemType.ballista]: {
       range: 550,
       // @Temporary
-      // arc: 2 * Math.PI
-      arc: Math.PI / 2.3
+      arc: 2 * Math.PI
+      // arc: Math.PI / 2.3
    },
    [ItemType.sling_turret]: {
       range: 300,
